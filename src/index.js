2a66a5812560b52abf48c3b8a7e1eb70bfa98dc0
Bemal <bimal163@gmail.com>
2024-12-15 00:00:25 +0400

    fixbug in env

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import {Telegraf} from 'telegraf';
-import {message} from "telegraf/filters";
-import config from 'config';
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import dotenv from "dotenv";
 
-const bot = new Telegraf(config.get(process.env.BOT_API_KEY));
+dotenv.config()
+const bot = new Telegraf(process.env.BOT_API_KEY);
 
 bot.on(message("voice"), async (ctx) => {
-    await ctx.reply(JSON.stringify(ctx.message.voice, null, 1))
-})
+  try {
+    const linkVoice = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    console.log(linkVoice.href)
+    await ctx.reply(JSON.stringify(linkVoice, null, 1));
+  } catch (e) {
+    console.log(`Its error`, e.message);
+  }
+});
 
-bot.on("text", async ctx => {
-    await ctx.reply(JSON.stringify(ctx.message, null, 1))
-})
+bot.on("text", async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 1));
+});
 
 bot.command("start", async (ctx) => {
-    await ctx.reply(JSON.stringify(ctx.message, null, 1));
-})
+  await ctx.reply(JSON.stringify(ctx.message, null, 1));
+});
 
-bot.launch()
+bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
