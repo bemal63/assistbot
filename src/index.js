4a43fa50e6eac66de2c806a4e79ddd6daf3216ce
Bemal <bimal-c163@yandex.ru>
2024-12-14 12:56:13 +0300

    create default config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Work...')
+import {Telegraf} from 'telegraf';
+import {message} from "telegraf/filters";
+import config from 'config';
+
+const bot = new Telegraf(config.get("TELEGRAMM_TOKEN"));
+
+bot.on(message("voice"), async (ctx) => {
+    await ctx.reply(JSON.stringify(ctx.message.voice, null, 1))
+})
+
+bot.on("text", async ctx => {
+    await ctx.reply(JSON.stringify(ctx.message, null, 1))
+})
+
+bot.command("start", async (ctx) => {
+    await ctx.reply(JSON.stringify(ctx.message, null, 1));
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
